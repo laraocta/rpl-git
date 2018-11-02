f2fff51370a997e709b551e7679816766d9b497e
i16052 <i16052@LAB02-14.ftis.unpar>
2018-11-02 14:34:26 +0700

    tambahkurang

--- rpl-git/src/Calculator.java
+++ rpl-git/src/Calculator.java
@@ -15,13 +15,13 @@ public class Calculator {
     }
     
     public float plus(float a, float b){
-        
+        return a+b;
         
     }
     
     public float minus(float a, float b){
         
-        
+        return a-b;
     }
     
     public float multiply(float a, float b){
